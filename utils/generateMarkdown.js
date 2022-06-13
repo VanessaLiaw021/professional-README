bb4d0c88ea47e059e45310e663c26649db410806
VanessaLiaw021 <vliaw021@gmail.com>
2022-06-13 12:45:37 -0400

    added the layout of a README file in the generateMarkdown function

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -14,6 +14,37 @@ function renderLicenseSection(license) {}
 function generateMarkdown(data) {
   return `# ${data.title}
 
+## Table of Contents
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributor](#contributing)
+- [Test](#test)
+- [Questions](#questions)
+
+## Description 
+${data.description}
+
+## Installation
+${data.installation}
+
+## Usage
+${data.usage}
+
+## License 
+${data.license}
+
+## Contributing
+${data.contributor}
+
+## Test
+${data.test}
+
+## Questions
+If you have any questions or concerns, email me at ${data.email}
+
+If you want to view my profile on GitHub, go to [${data.username}](https://github.com/${data.username})
 `;
 }
 
